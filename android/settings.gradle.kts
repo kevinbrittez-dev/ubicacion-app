41f886b4f50898402434205591df8f5da7e33c20
kevinbrittez-dev <kevinbrittez@gmail.com>
2026-03-22 17:58:22 -0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,12 +1,11 @@
 pluginManagement {
     val flutterSdkPath = System.getenv("FLUTTER_ROOT")
         ?: run {
-            val properties = java.util.Properties()
-            val localPropertiesFile = file("local.properties")
-            if (localPropertiesFile.exists()) {
-                localPropertiesFile.inputStream().use { properties.load(it) }
-            }
-            properties.getProperty("flutter.sdk") ?: error("Flutter SDK not found")
+            val props = java.util.Properties()
+            file("local.properties").takeIf { it.exists() }
+                ?.inputStream()?.use { props.load(it) }
+            props.getProperty("flutter.sdk")
+                ?: error("Flutter SDK not found. Set FLUTTER_ROOT or flutter.sdk in local.properties")
         }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
@@ -20,8 +19,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
