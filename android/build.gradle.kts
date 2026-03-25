a500586d901dbf563b1dbca9b432e3716dbdd9d1
kevinbrittez-dev <kevinbrittez@gmail.com>
2026-03-25 10:57:14 -0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,15 +1,24 @@
 buildscript {
-repositories {
-google()
-mavenCentral()
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.1")
+    }
 }
-dependencies {
-classpath("com.google.gms:google-services:4.4.1")
-}
-}
+
 allprojects {
-repositories {
-google()
-mavenCentral()
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
